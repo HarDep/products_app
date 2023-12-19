5beb9c39e8f674dc863d9b6f7bf92e8a827731ad
HarDep <haroldralvaradol@gmail.com>
2023-12-19 02:26:42 -0500

    Add cart provider

---
+++ lib/presentation/providers/cart_provider.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:products_app/domain/models/product.dart';
+import 'package:products_app/domain/models/product_cart.dart';
+
+class CartProvider extends ChangeNotifier {
+  List<ProductCart> cart = [];
+  int totalItems = 0;
+  double totalPrice = 0;
+
+  void add(Product product) {
+    bool isFound = false;
+    for (final prod in cart) {
+      if (prod.product.name == product.name) {
+        prod.amount++;
+        isFound = true;
+        break;
+      }
+    }
+    if (!isFound) {
+      cart.add(ProductCart(product: product));
+    }
+    totalItems++;
+    totalPrice += product.price;
+    notifyListeners();
+  }
+
+  void increment(ProductCart productCart) {
+    productCart.amount++;
+    totalItems++;
+    totalPrice += productCart.product.price;
+    notifyListeners();
+  }
+
+  void decrement(ProductCart productCart) {
+    if (productCart.amount > 1) {
+      productCart.amount--;
+      totalItems--;
+      totalPrice -= productCart.product.price;
+      notifyListeners();
+      return;
+    }
+    delete(productCart);
+  }
+
+  void delete(ProductCart productCart) {
+    totalItems--;
+    totalPrice -= productCart.product.price;
+    cart.remove(productCart);
+    notifyListeners();
+  }
+}
